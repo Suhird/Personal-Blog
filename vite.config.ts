18df4bc177f44ee6d2a1d6540ae81ec2dd6ec554
Suhird <suhirdsingh92@gmail.com>
2025-05-12 19:40:21 -0400

    changed base config

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,24 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  // base: "Personal-Blog", // ⬅️ Replace with your actual GitHub repo name
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   // base: "Personal-Blog", // ⬅️ Replace with your actual GitHub repo name
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [react(), mode === "development" && componentTagger()].filter(
+//     Boolean
+//   ),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
+export default defineConfig({
+  base: "Personal-Blog", // 🔁 Replace with your actual repo name
+  plugins: [react()],
+});
